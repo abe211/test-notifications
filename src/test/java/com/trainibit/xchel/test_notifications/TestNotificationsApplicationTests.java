2f093f32f6266e5bfddfebd60d88b3d007b53eea
Abelardo <2117031374@upfim.edu.mx>
2025-02-04 13:49:30 -0600

    esqueleto

---
+++ src/test/java/com/trainibit/xchel/test_notifications/TestNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainibit.xchel.test_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
